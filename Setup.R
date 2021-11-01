1a24028568bba6276a183002d2d2d80d1a55c258
metzgersimon <Simon.Metzger98@gmail.com>
2021-11-01 09:54:07 +0100

    Adjust setup file
    Added some more libraries and sourced the Functions folder to get all R-files which are in that folder.

--- Setup.R
+++ Setup.R
@@ -1,16 +1,24 @@
 #### Loading libraries needed ####
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("httr")) install.packages("httr")
+if (!require("lubridate")) install.packages("lubridate")
+if (!require("miceadds")) install.packages("miceadds")
 if (!require("rlist")) install.packages("rlist")
+if (!require("rvest")) install.packages("rvest")
 if (!require("stringr")) install.packages("stringr")
 if (!require("tidyr")) install.packages("tidyr")
 
 library(dplyr)
 library(httr)
+library(lubridate)
+library(miceadds)
 library(rlist)
+library(rvest)
 library(stringr)
 library(tidyr)
 
 
 # Source script containing all API keys needed
 source("API_keys.R")
+# Source all R-files in the Functions folder
+source.all("Functions/", grepstring="\\.R")
