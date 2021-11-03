cd47a2241cf572f51e5a75a443643d3dae6732e5
metzgersimon <Simon.Metzger98@gmail.com>
2021-11-03 15:45:49 +0100

    Add worldfootball package
    Added the installation and loading of the worldfootballR package to the Setup.R file

--- Setup.R
+++ Setup.R
@@ -1,5 +1,6 @@
 #### Loading libraries needed ####
 if (!require("dplyr")) install.packages("dplyr")
+if (!require("devtools")) install.packages("devtools")
 if (!require("httr")) install.packages("httr")
 if (!require("lubridate")) install.packages("lubridate")
 if (!require("miceadds")) install.packages("miceadds")
@@ -7,6 +8,8 @@ if (!require("rlist")) install.packages("rlist")
 if (!require("rvest")) install.packages("rvest")
 if (!require("stringr")) install.packages("stringr")
 if (!require("tidyr")) install.packages("tidyr")
+if (!require("worldfootballR")) devtools::install_github("JaseZiv/worldfootballR")
+
 
 library(dplyr)
 library(httr)
@@ -16,9 +19,12 @@ library(rlist)
 library(rvest)
 library(stringr)
 library(tidyr)
+library(worldfootballR)
 
 
 # Source script containing all API keys needed
 source("API_keys.R")
 # Source all R-files in the Functions folder
 source.all("Functions/", grepstring="\\.R")
+
+
