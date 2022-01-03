70efa689d411207b4bd8c02eb610adebc3805164
metzgersimon <simon.metzger98@gmail.com>
2022-01-03 13:47:11 +0100

    Added new menu items for the prediction, namely investment which should later on show the performance of the model. Removed the previous servers of the prediction menu

---
+++ Shiny/prediction_investment_server.R
@@ -0,0 +1,4 @@
+# subserver for the investment tab in the prediction menu item
+prediction_investment_server <- function(input, output, session){
+  
+}
--- Shiny/prediction_server.R
+++
@@ -1,3 +0,0 @@
-prediction_server <- function(input, output, session){
-  
-}
