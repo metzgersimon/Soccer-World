f91344f52a999c097b89cc921512ef4559b51b8f
Anton Hoehl <anton.hoehl@student.uni-tuebingen.de>
2022-02-07 18:53:20 +0100

    wrote pusher function for sql server

---
+++ Functions/Pusher.R
@@ -0,0 +1,24 @@
+source("Setup.R")
+sql_pusher <- function (folder = "./Clean Data files/") {
+  loadRData <- function(fileName){
+    #loads an RData file, and returns it
+    load(fileName)
+    get(ls()[ls() != "fileName"])
+  }
+  
+  con <- dbConnect(RMariaDB::MariaDB(), 
+                   host='127.0.0.1',
+                   dbname='Soccer_Prediction_Data',
+                   username='root',
+                   password='my-secret-pw')
+  
+  file_names = list.files(path = substr(folder, 1, nchar(folder)-1))
+  
+  for (naming in file_names) {
+    interm <- loadRData(paste(folder, naming, sep = ""))
+    print(naming)
+    dbWriteTable(con,name = naming, interm, overwrite = TRUE )
+  }
+  
+}
+
