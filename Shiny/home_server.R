e73d5e77b8c5351f27fcca31fabf12675db9fdb2
Jingwen <jinxu0120@gmail.com>
2022-02-10 17:13:10 +0100

    update home tab

--- Shiny/home_server.R
+++ Shiny/home_server.R
@@ -1,36 +1,43 @@
 # subserver for the home menu item
 home_server <- function(input, output, session){
+
   
   output$map <- renderLeaflet({   
     venues_with_coordinates_home <-
       all_leagues_venue_information %>% mutate(
         league = if_else(all_leagues_venue_information$league_id == 78, "Bundesliga 1",if_else(venues_with_coordinates$league_id == 79, "Bundesliga 2", 
-                        if_else(all_leagues_venue_information$league_id == 39,"Premier League",
-                        if_else(all_leagues_venue_information$league_id == 61,  "Ligue 1","none")))), 
+                                                                                               if_else(all_leagues_venue_information$league_id == 39,"Premier League",
+                                                                                                       if_else(all_leagues_venue_information$league_id == 61,  "Ligue 1","none")))), 
         popup_text = paste0("<center>",
                             #Setting up poopup info
                             "</br><b>Team Name</b>: ", 
-          team_name,
-          "</br><b>Country</b>: ",
-          country,
-          "</br><b>Founded Year</b>: ",
-          founded,
-          "</br><b>Venue Name</b>: ",
-          venue_name
+                            team_name,
+                            "</br><b>Country</b>: ",
+                            country,
+                            "</br><b>Founded Year</b>: ",
+                            founded,
+                            "</br><b>Venue Name</b>: ",
+                            venue_name
         )
       ) %>%
       filter(league == input$leagues)
     
+    # make the icon to be the team flag
+    flagIcon <- makeIcon(
+      iconUrl =  venues_with_coordinates_home$logo,
+      iconWidth = 30, iconHeight = 30,
+      shadowWidth = 10, shadowHeight = 10
+    )
     
-   # map test
-  leaflet(venues_with_coordinates_home) %>%
-    addProviderTiles(providers$Esri.WorldTopoMap) %>%
-    addMarkers(~longitude, ~latitude, 
-               label = ~team_name, 
-              # icon = ~logo,
-               labelOptions = labelOptions(textsize = "12px"),
-               popup = ~popup_text)
-  
+    # map test
+    leaflet(venues_with_coordinates_home) %>%
+      addProviderTiles(providers$Esri.WorldTopoMap) %>% clearMarkerClusters() %>%
+      addMarkers(~longitude, ~latitude, 
+                 label = ~team_name, 
+                 icon = flagIcon,
+                 labelOptions = labelOptions(textsize = "12px"),
+                 popup = ~popup_text)
+    
   })
   
   observe({
