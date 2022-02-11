8436c244e74302a017b9e5ac4f81898f03047ca0
metzgersimon <Simon.Metzger98@gmail.com>
2022-02-11 23:25:19 +0100

    Update the get_data scripts for the API and TM to be able to call each part by its own

--- Get_data_API.R
+++ Get_data_API.R
@@ -1,65 +1,197 @@
-########### get the new fixtures #########
-# current matches in the data base
-current_season <- 2021
-
-
-# write_new_data_in_db <- function()
-
-buli_matches <- tbl(con, "buli_matches_2010_2021") %>%
-  # filter(league_season == current_season) %>%
-  data.frame()
-
-# check for the latest available matchday
-latest_matchday <- buli_matches %>%
-  filter(league_season == current_season,
-         !is.na(fulltime_score_home) &
-           !is.na(fulltime_score_away)) %>%
-  filter(league_round == max(league_round)) %>%
-  select(league_round) %>%
-  pull() %>%
-  unique()
-
-# get the newest matchday
-newest_matchday <- latest_matchday + 1
-
-
-newest_matchday_data <- get_fixtures_in_league_by_season(league_id = 78,
-                                                         season = current_season,
-                                                         matchday = newest_matchday)
-
-buli_matches_new <- buli_matches %>%
-  filter(league_season == current_season,
-         league_round != newest_matchday) %>%
-  bind_rows(newest_matchday_data) %>%
-  arrange(league_round, fixture_date, fixture_time)
-
-buli_matches_2010_2021 <- buli_matches %>%
-  filter(league_season != current_season) %>%
-  bind_rows(buli_matches_new)
-
-# write the new data in the data base (overwrite the old one)
-dbWriteTable(con, name = "buli_matches_2010_2021", buli_matches_2010_2021,
-             overwrite = TRUE)
-
-
-
-############## get new match stats ##################
-
-# get all the match stats for the new matches
-newest_matchday_stats <- NULL
-
-buli_match_stats <- tbl(con, "buli_match_stats_2015_2021") %>%
-  # filter(league_season == current_season) %>%
-  data.frame()
-
-for(i in 1:nrow(newest_matchday_data)){
-  match_i_id <- newest_matchday_data$fixture_id[i]
-  match_i_stats <- get_fixture_stats(match_i_id)
+########## get newest fixture stats ############
+get_new_match_stats_API <- function(){
+  # extract the max available match days for each league
+  max_matchdays <- all_leagues_fixture_stats %>%
+    filter(season == max(season)) %>%
+    group_by(league_id) %>%
+    summarize(max_matchday = max(matchday))
   
-  newest_matchday_stats <- bind_rows(newest_matchday_stats,
-                                     match_i_stats)
+  # compute the new matchday we want to get data for
+  newest_matchdays <- max_matchdays %>%
+    mutate(max_matchday = max_matchday + 1)
   
+  # extract all the fixture ids for all leagues on the newest matchday
+  fixture_ids <- NULL
+  
+  # iterate over all leagues in the newest_matchdays frame
+  for(i in 1:nrow(newest_matchdays)){
+    # for every league get all the fixture ids for the new matchday
+    curr_fixture_ids <- all_leagues_matches %>%
+      filter(league_season == max(league_season),
+             league_id == newest_matchdays$league_id[i],
+             league_round == newest_matchdays$max_matchday[i])
+    
+    # bind them together with the fixture_ids frame for all leagues
+    fixture_ids <- bind_rows(fixture_ids,
+                             curr_fixture_ids)
+  }
+  
+  # create a variable to store the newly extracted fixture stats
+  all_leagues_fixture_stats_new <- NULL
+  
+  # iterate over all matches we extracted above
+  for(i in 1:length(fixture_ids$fixture_id)){
+    # extract the fixture stats of the current match
+    curr_fixture_stats <- get_fixture_stats(fixture_ids$fixture_id[i])
+  
+    # bind them together with the fixture_ids frame for all matches
+    all_leagues_fixture_stats_new <- bind_rows(all_leagues_fixture_stats_new,
+                                               curr_fixture_stats)
+  }
+  
+  # finally, if we have actual stats data, we want to write it to the data base
+  if(nrow(all_leagues_fixture_stats_new) != 0){
+    
+    dbWriteTable(con, "all_leagues_fixture_stats", all_leagues_fixture_stats_new,
+                 overwrite = FALSE, append = TRUE)
+  }
 }
 
-buli_match_stats <- bind_rows(buli_match_stats,
-                              newest_matchday_stats)
+
+
+
+########## get newest player stats ############
+get_new_player_stats_API <- function(){
+  # extract the max available match days for each league
+  max_matchdays <- all_leagues_player_stats %>%
+    filter(league_season == max(league_season)) %>%
+    group_by(league_id) %>%
+    summarize(max_matchday = max(league_round))
+  
+  # compute the new matchday we want to get data for
+  newest_matchdays <- max_matchdays %>%
+    mutate(max_matchday = max_matchday + 1)
+  
+  # extract all the fixture ids for all leagues on the newest matchday
+  fixture_ids <- NULL
+  
+  # iterate over all leagues in the newest_matchdays frame
+  for(i in 1:nrow(newest_matchdays)){
+    # for every league get all the fixture ids for the new matchday
+    curr_fixture_ids <- all_leagues_matches %>%
+      filter(league_season == max(league_season),
+             league_id == newest_matchdays$league_id[i],
+             league_round == newest_matchdays$max_matchday[i])
+    
+    # bind them together with the fixture_ids frame for all leagues
+    fixture_ids <- bind_rows(fixture_ids,
+                             curr_fixture_ids)
+  }
+  
+  # create a variable to store the newly extracted player stats
+  all_leagues_player_stats_new <- NULL
+  
+  # iterate over all matches we extracted above
+  for(i in 1:length(fixture_ids$fixture_id)){
+    # extract the player stats of the current match
+    curr_player_stats <- get_player_stats_fixture(fixture_ids$fixture_id[i])
+    
+    # bind them together with the fixture_ids frame for all matches
+    all_leagues_player_stats_new <- bind_rows(all_leagues_player_stats_new,
+                                              curr_player_stats)
+  }
+  
+  # finally, if we have actual stats data, we want to write it to the data base
+  if(nrow(all_leagues_player_stats_new) != 0){
+    
+    dbWriteTable(con, "all_leagues_player_stats", all_leagues_player_stats_new,
+                 overwrite = FALSE, append = TRUE)
+  }
+}
+
+
+
+
+
+########## get newest team stats ############
+get_new_club_stats_API <- function(){
+  # extract the max available match days for each league
+  max_matchdays <- all_leagues_club_stats %>%
+    filter(league_season == max(league_season)) %>%
+    group_by(league_id) %>%
+    summarize(max_matchday = max(matchday))
+  
+  # extract the current (and max) season
+  max_season <- all_leagues_club_stats %>%
+    summarize(max_season = max(league_season)) %>%
+    pull()
+  
+  # compute the new matchday we want to get data for
+  newest_matchdays <- max_matchdays %>%
+    mutate(new_matchday = max_matchday + 1)
+  
+  
+  # get all the teams that we want to look at
+  league_team_ids <- all_leagues_club_stats %>%
+    # only look in the current season
+    filter(league_season == max(league_season)) %>%
+    # select only the league and team id 
+    select(league_id, team_id) %>%
+    # only use distinct values
+    distinct()
+  
+  
+  # for those extracted league and team ids we now have to find the date of the
+  # last match (i.e., the new match that is not already in the data base)
+  all_team_match_dates <- NULL
+  
+  for(i in 1:nrow(league_team_ids)){
+    # extract the matchday we want to get data for given our current league
+    new_matchday_for_curr_league <- newest_matchdays %>%
+      filter(league_id == league_team_ids$league_id[i]) %>%
+      select(new_matchday) %>% 
+      pull()
+    
+    # extract the match date of the last match for the current team
+    curr_team_match_date <- all_leagues_matches %>%
+      filter(league_id == league_team_ids$league_id[i],
+             club_id_home == league_team_ids$team_id[i] |
+               club_id_away == league_team_ids$team_id[i],
+             league_season == max_season,
+             league_round == new_matchday_for_curr_league) %>%
+      # get only the match date
+      select(league_id, fixture_date) %>%
+      # add the team id as variable
+      mutate(team_id = league_team_ids$team_id[i])
+    
+    
+    # append the match date of the current team to the overall frame
+    all_team_match_dates <- bind_rows(all_team_match_dates,
+                                      curr_team_match_date)
+  }
+  
+  
+  # create an variable to store the team stats of all leagues
+  all_leagues_club_stats_new <- NULL
+  
+  # iterate over all league-team pairs and get the team stats for these pairs
+  for(i in 1:nrow(all_team_match_dates)){
+    # extract and store the ids of the current league and team
+    # and its last (most recent) match date
+    curr_league_id <- all_team_match_dates$league_id[i]
+    curr_team_id <- all_team_match_dates$team_id[i]
+    curr_fixture_date <- all_team_match_dates$fixture_date[i]
+    
+    # extract the team stats of the current team
+    curr_team_club_stats <- get_team_stats_in_league_by_season(curr_league_id,
+                                                               curr_team_id,
+                                                               max_season,
+                                                               curr_fixture_date)
+    
+    # bind together the curr team data and the overall data
+    all_leagues_club_stats_new <- bind_rows(all_leagues_club_stats_new,
+                                            curr_team_club_stats)
+                                                               
+            
+  }
+  
+  
+  # finally, if we have actual stats data, we want to write it to the data base
+  if(nrow(all_leagues_club_stats_new) != 0){
+    
+    dbWriteTable(con, "all_leagues_club_stats", all_leagues_club_stats_new,
+                 overwrite = FALSE, append = TRUE)
+  }
+}
+
+
--- Get_data_Tm.R
+++ Get_data_Tm.R
@@ -1,30 +1,97 @@
+### script should check for updates on the Transfermarkt.com web page for 
+# the market values, the fifa top nations rating, daily ###
 
-season <- 2021
-matchday <- 21
+########## get market values from TM ############
+# get the max available date for this data set in our data base
+max_date <- all_leagues_market_values_over_time %>%
+  summarize(date = max(date)) %>%
+  select(date) %>%
+  pull()
 
-newest_matchday_info <- get_fixture_detailed_info(league = "bundesliga",
-                                                  league_id = "L1",
-                                                  matchday = matchday,
-                                                  port = 2222L)
+# create the vectors of the league names and ids
+leagues <- c("bundesliga", "2-bundesliga", "premier-league", "ligue-1")
+league_ids <- c("L1", "L2", "GB1", "FR1")
 
+# do the actual scraping 
+all_leagues_market_values_new <- get_market_values_over_time_all_leagues(leagues,
+                                                                         league_ids,
+                                                                         max_date)
 
-
-seasons <- c(2015:2019)
-ports <- c(2040L:2050L)
-
-fixture_info_tm_2010_2021 <- NULL
-
-for(i in 1:length(seasons)){
-  print(paste0("Current season: ", seasons[i]))
-  curr_infos <- get_fixture_detailed_info(league = "bundesliga",
-                                          league_id = "L1",
-                                          season = seasons[i],
-                                          port = ports[i])
+# if the all_leagues_market_values_new element is not null, i.e.,
+# there was no new data available, we write the new data (by appending it)
+# into the data base
+if(!is.null(all_leagues_market_values_new)){
   
-  fixture_info_tm_2010_2021 <- bind_rows(fixture_info_tm_2010_2021,
-                                         curr_infos)
-  
-  Sys.sleep(5)
-  
-  
+  dbWriteTable(con, "all_leagues_market_values_over_time", 
+               all_leagues_market_values_new,
+               overwrite = FALSE, append = TRUE)
 }
+
+
+
+
+########## get squads ############
+# get the newest available season
+max_season <- max(all_leagues_tm_squads$season)
+
+# create the vectors of the league names and ids
+leagues <- c("bundesliga", "2-bundesliga", "premier-league", "ligue-1")
+league_ids <- c("L1", "L2", "GB1", "FR1")
+
+# do the actual scraping 
+all_leagues_squads_new <- get_squads_by_season_all_leagues(leagues,
+                                                           league_ids,
+                                                           max_season)
+
+
+# filter for only those matches that are not already in the data base
+all_leagues_squads_new_test <- all_leagues_tm_squads %>%
+  # filter only for the current season
+  filter(season == max_season,
+         # drop the serie a and la liga
+         league %in% leagues) %>%
+  # use an anti join to get all of those matches that are not in the data base
+  anti_join(all_leagues_squads_new, 
+            by = c("league", "season", "club")) %>%
+  # filter for only those that are in the past
+  filter(date < Sys.Date())
+
+# if the all_leagues_market_values_new element is not null, i.e.,
+# there was new data available, we write the new data (by appending it)
+# into the data base
+if(!is.null(all_leagues_market_values_new)){
+  
+  dbWriteTable(con, "all_leagues_market_values_over_time", 
+               all_leagues_market_values_new,
+               overwrite = FALSE, append = TRUE)
+}
+
+
+
+
+########## get fifa world ranking top nations ############
+# get the newest date in the data base
+max_date <- fifa_top_15_nations %>%
+  filter(date == max(date)) %>%
+  select(date) %>%
+  pull() %>%
+  unique()
+
+# extract the new fifa top 15 ranking table
+fifa_top_15_nations_new <- get_fifa_world_ranking_top_nations(max_date,
+                                                              top_nations = 15)
+
+
+# if the fifa_top_15_nations_new element is not null, i.e.,
+# there was new data available, we write the new data (by appending it)
+# into the data base
+if(!is.null(fifa_top_15_nations_new)){
+  
+  dbWriteTable(con, "fifa_top_15_nations", 
+               fifa_top_15_nations_new,
+               overwrite = FALSE, append = TRUE)
+}
+
+
+
+
