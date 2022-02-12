ff609d8abfcf86ea97e72c23ae44472d80c1c221
Anton Hoehl <anton.hoehl@student.uni-tuebingen.de>
2022-02-12 16:40:26 +0100

    set lineup creates cron jobs on a given day lineup_cron is triggered by said cron jobs

---
+++ Scheduled/lineup_cron.R
@@ -0,0 +1,31 @@
+source("Setup.R")
+
+# load the timetable
+load("./Scheduled/timeslots.RData")
+
+# select closest uscraped timeslots
+current <- timeslots %>% 
+              filter(fixture_POSIXct == as.character(
+                                            min(timeslots$fixture_POSIXct)))
+
+# remove selected timeslots from table and save it 
+timeslots <- anti_join(timeslots, current)
+save(timeslots, file = "./Scheduled/timeslots.RData")
+
+# receive data for selected ids
+new_lineups <- data.frame()
+for (id in current$fixture_id) {
+  new_lineups <- rbind(new_lineups, get_fixture_lineups(id))
+}
+
+# connect to database
+con <- dbConnect(RMariaDB::MariaDB(), 
+                 host='127.0.0.1',
+                 dbname='Soccer_Prediction_Data',
+                 username='root',
+                 password='my-secret-pw')
+
+dbWriteTable(con, "all_leagues_lineups", new_lineups,
+             overwrite = FALSE, append = TRUE)
+
+dbDisconnect(con)
---
+++ Scheduled/set_lineup_timer.R
@@ -0,0 +1,29 @@
+source("Setup.R")
+
+# get the matches actually scheduled for today
+leagues_today <- get_leagues_playing_today()
+matches_today <- get_match_data_todays_games(leagues_today)
+timeslots <- get_times_for_lineup_scraping(matches_today)
+
+save(timeslots, file = "./Scheduled/timeslots.RData")
+
+# delete cron tabs from the day before
+cron_clear(ask = FALSE)
+
+# initialise new cronR tabs for today
+
+slots <- timeslots %>% 
+          ungroup() %>%
+          select(lineup_time1_cronjob) %>%
+          unique()
+
+for (sl in slots) {
+  # add the right syntax for skript
+  cron_add(command = cron_rscript("./Scheduled/lineup_cron.R"), 
+           frequency = sl, 
+           ask = FALSE)
+}
+
+
+
+
