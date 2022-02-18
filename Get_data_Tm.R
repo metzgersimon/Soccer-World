980192e24009fccd189bb170f61df2e994bdd8d2
Anton Hoehl <anton.hoehl@student.uni-tuebingen.de>
2022-02-18 19:14:33 +0100

    changed names

--- Get_data_Tm.R
+++ Get_data_Tm.R
@@ -39,13 +39,14 @@ leagues <- c("bundesliga", "2-bundesliga", "premier-league", "ligue-1")
 league_ids <- c("L1", "L2", "GB1", "FR1")
 
 # do the actual scraping 
+# throws an error
 all_leagues_squads_new <- get_squads_by_season_all_leagues(leagues,
                                                            league_ids,
                                                            max_season)
 
 
 # filter for only those matches that are not already in the data base
-all_leagues_squads_new_test <- all_leagues_tm_squads %>%
+all_leagues_squads_new <- all_leagues_tm_squads %>%
   # filter only for the current season
   filter(season == max_season,
          # drop the serie a and la liga
@@ -59,38 +60,38 @@ all_leagues_squads_new_test <- all_leagues_tm_squads %>%
 # if the all_leagues_market_values_new element is not null, i.e.,
 # there was new data available, we write the new data (by appending it)
 # into the data base
-if(!is.null(all_leagues_market_values_new)){
+if(!is.null(all_leagues_squads_new)){
   
-  dbWriteTable(con, "all_leagues_market_values_over_time", 
-               all_leagues_market_values_new,
+  dbWriteTable(con, "all_leagues_squads_tm", 
+               all_leagues_squads_new,
                overwrite = FALSE, append = TRUE)
 }
 
 
 
 
-########## get fifa world ranking top nations ############
-# get the newest date in the data base
-max_date <- fifa_top_15_nations %>%
-  filter(date == max(date)) %>%
-  select(date) %>%
-  pull() %>%
-  unique()
-
-# extract the new fifa top 15 ranking table
-fifa_top_15_nations_new <- get_fifa_world_ranking_top_nations(max_date,
-                                                              top_nations = 15)
-
-
-# if the fifa_top_15_nations_new element is not null, i.e.,
-# there was new data available, we write the new data (by appending it)
-# into the data base
-if(!is.null(fifa_top_15_nations_new)){
-  
-  dbWriteTable(con, "fifa_top_15_nations", 
-               fifa_top_15_nations_new,
-               overwrite = FALSE, append = TRUE)
-}
+# ########## get fifa world ranking top nations ############
+# # get the newest date in the data base
+# max_date <- fifa_top_15_nations %>%
+#   filter(date == max(date)) %>%
+#   select(date) %>%
+#   pull() %>%
+#   unique()
+# 
+# # extract the new fifa top 15 ranking table
+# fifa_top_15_nations_new <- get_fifa_world_ranking_top_nations(max_date,
+#                                                               top_nations = 15)
+# 
+# 
+# # if the fifa_top_15_nations_new element is not null, i.e.,
+# # there was new data available, we write the new data (by appending it)
+# # into the data base
+# if(!is.null(fifa_top_15_nations_new)){
+#   
+#   dbWriteTable(con, "fifa_top_15_nations", 
+#                fifa_top_15_nations_new,
+#                overwrite = FALSE, append = TRUE)
+# }
 
 
 
