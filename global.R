72aed6b7fa97e8a105218ba5a364738e8d411225
Anton Hoehl <anton.hoehl@student.uni-tuebingen.de>
2022-02-19 23:22:59 +0100

    tm name

--- global.R
+++ global.R
@@ -33,7 +33,7 @@ all_leagues_spi_538_available_matches <- tbl(con, "all_leagues_spi_538") %>%
 all_leagues_squads_tm <- tbl(con, "all_leagues_squads_tm") %>% data.frame()
 all_leagues_market_values_over_time <- tbl(con, "all_leagues_market_values_over_time") %>%
   data.frame()
-all_infos_club <- inner_join(all_leagues_tm_squads, unique(all_leagues_matches[,c(2,3,19)]), 
+all_infos_club <- inner_join(all_leagues_squads_tm, unique(all_leagues_matches[,c(2,3,19)]), 
                              by=c("club"="club_name_home", "league"="league_name"))
 
 all_leagues_club_stats <- tbl(con, "all_leagues_club_stats") %>%
