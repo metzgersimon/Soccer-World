a99211ebfbb0188e92c95db085466d16a732a045
Anton Hoehl <anton.hoehl@student.uni-tuebingen.de>
2022-02-21 23:23:03 +0100

    tilde

--- Functions/FootballAPI.R
+++ Functions/FootballAPI.R
@@ -1197,7 +1197,7 @@ get_player_stats_fixture <- function(fixture_id){
         current_player_compl <- api_football_fixtures_general_complete_check(
           current_player, "player_stats_fixture") %>%
           # fill all NA values with 0
-          mutate(across(.cols = everything(), ~as.character),
+          mutate(across(.cols = everything(), as.character),
             across(.cols = everything(), ~replace_na(.x, "0")),
                  # convert most of the variables dynamically into numeric variables
                  across(c(contains("offsides"), contains("shots"),
