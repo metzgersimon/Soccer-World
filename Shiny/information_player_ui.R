7e467c7f41f7238e3b99dd06b2e7e12c7082f6b1
Jingwen <jinxu0120@gmail.com>
2022-02-22 18:21:41 +0100

    add comments

--- Shiny/information_player_ui.R
+++ Shiny/information_player_ui.R
@@ -116,7 +116,7 @@ tab_information_player_ui <- function() {
                                  width = "100%",
                                  height = 300
                                ) %>%
-                                 withSpinner(color = "black")
+                                 withSpinner(color = "black") # loading icon
                              )
                            ),
                            column(
